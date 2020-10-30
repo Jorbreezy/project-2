c21953caee350b912fd59f81dd7c964738a8358e
Jordan Kelly <jordan.w.kelly@gmail.com>
2020-10-30 12:53:16 -0400

    Rendered react into html working, refactored code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Chores from './components/Chores.jsx';
+
 const App = () => (
-  <div><h1>Hello World</h1></div>
+  <div>
+    <h1>Hello World</h1>
+    <Chores />
+  </div>
 );
 
 export default App;
---
+++ src/components/Chores.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chores = () => (
+  <ul>
+    <li>Do laundry</li>
+    <li>Get groceries</li>
+    <li>Clean room</li>
+  </ul>
+);
+
+export default Chores;
